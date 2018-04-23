891fb2ec02be2a90f640acdf599f7eb2cf1641d3
Joshua Laesch <joshua.laesch@gmail.com>
2018-04-23 11:05:59 -0400

    firebase configured for the app

--- PasswordKeeperLaesch/AppDelegate.swift
+++ PasswordKeeperLaesch/AppDelegate.swift
@@ -1,46 +1,64 @@
 //
 //  AppDelegate.swift
-//  PasswordKeeperLaesch
+//  Password Keeper
 //
-//  Created by CSSE Department on 4/19/18.
-//  Copyright © 2018 Rose-Hulman. All rights reserved.
+//  Created by David Fisher on 4/11/18.
+//  Copyright © 2018 David Fisher. All rights reserved.
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+  var window: UIWindow?
 
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    
+    FirebaseApp.configure()
+    
+    // To give the iOS status bar light icons & text
+    UIApplication.shared.statusBarStyle = .lightContent
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    // Programatically initialize the first view controller.
+    window = UIWindow(frame: UIScreen.main.bounds)
+
+    let showLoginScreen = true
+    if showLoginScreen {
+      showLoginViewController();
+    } else {
+      showPasswordViewController();
     }
+    window?.makeKeyAndVisible()
+    return true
+  }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
+  func handleLogin() {
+    print("TODO: Implement sign in from the AppDelegate")
+  }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
+  @objc func handleLogout() {
+    print("TODO: Implement sign out from the AppDelegate")
+  }
 
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
+  func showLoginViewController() {
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    window!.rootViewController = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
+  }
 
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+  func showPasswordViewController() {
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    let passwordViewController = storyboard.instantiateViewController(withIdentifier: "PasswordViewController")
+    window!.rootViewController = AppNavBar(rootViewController: passwordViewController)
+  }
 }
 
+
+extension UIViewController {
+  var appDelegate : AppDelegate {
+    get {
+      return UIApplication.shared.delegate as! AppDelegate
+    }
+  }
+}
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+
+target 'PasswordKeeperLaesch' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Password Keeper
+  pod 'Material', '~> 2.14.0'
+  pod 'FoldingCell', '~> 3.0.6'
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Firestore'
+
+end
